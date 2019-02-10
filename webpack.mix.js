45e2fd79346e87fa1971b300a3c1a3cfa0de02fc
rivers-lis <rivers86lis@gmail.com>
2019-02-10 14:17:15 +0300

    create AdminLTE

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,59 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+mix.styles([
+    'resources/admin/bootstrap/css/bootstrap.min.css',
+    'resources/admin/font-awesome/4.5.0/css/font-awesome.min.css',
+    'resources/admin/ionicons/2.0.1/css/ionicons.min.css',
+    'resources/admin/plugins/iCheck/minimal/_all.css',
+    'resources/admin/plugins/datepicker/datepicker3.css',
+    'resources/admin/plugins/select2/select2.min.css',
+    'resources/admin/plugins/datatables/dataTables.bootstrap.css',
+    'resources/admin/dist/css/AdminLTE.min.css',
+    'resources/admin/dist/css/skins/_all-skins.min.css'
+], 'public/css/admin.css');
+
+mix.scripts([
+    'resources/admin/plugins/jQuery/jquery-2.2.3.min.js',
+    'resources/admin/bootstrap/js/bootstrap.min.js',
+    'resources/admin/plugins/select2/select2.full.min.js',
+    'resources/admin/plugins/datepicker/bootstrap-datepicker.js',
+    'resources/admin/plugins/datatables/jquery.dataTables.min.js',
+    'resources/admin/plugins/datatables/dataTables.bootstrap.min.js',
+    'resources/admin/plugins/slimScroll/jquery.slimscroll.min.js',
+    'resources/admin/plugins/fastclick/fastclick.js',
+    'resources/admin/plugins/iCheck/icheck.min.js',
+    'resources/admin/dist/js/app.min.js',
+    'resources/admin/dist/js/demo.js',
+    'resources/admin/dist/js/scripts.js'
+], 'public/js/admin.js');
+
+mix.copy('resources/admin/bootstrap/fonts', 'public/fonts');
+mix.copy('resources/admin/dist/fonts', 'public/fonts');
+mix.copy('resources/admin/dist/img', 'public/img');
+//mix.copy('resources/admin/plugins/iCheck/minimal/blue.png', 'public/css');
+
+/*
+mix.styles([
+    'resources/assets/front/css/bootstrap.min.css',
+    'resources/assets/front/css/font-awesome.min.css',
+    'resources/assets/front/css/animate.min.css',
+    'resources/assets/front/css/owl.carousel.css',
+    'resources/assets/front/css/owl.theme.css',
+    'resources/assets/front/css/owl.transitions.css',
+    'resources/assets/front/css/style.css',
+    'resources/assets/front/css/responsive.css'
+],'public/css/front.css');
+
+mix.scripts([
+    'resources/assets/front/js/jquery-1.11.3.min.js',
+    'resources/assets/front/js/bootstrap.min.js',
+    'resources/assets/front/js/owl.carousel.min.js',
+    'resources/assets/front/js/jquery.stickit.min.js',
+    'resources/assets/front/js/menu.js',
+    'resources/assets/front/js/scripts.js'
+], 'public/js/front.js');
+
+mix.copy('resources/assets/front/fonts', 'public/fonts');
+mix.copy('resources/assets/front/images', 'public/images');
+*/
